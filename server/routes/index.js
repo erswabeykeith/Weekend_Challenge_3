87da8ffc514326045350e7d1e6c11034cc0151c6
Liz Swabey-Keith <lizswabey-keith@Lizs-MacBook-Pro-2.local>
2016-11-11 16:47:13 -0600

    html works on webpage

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 router.get('/*', function(req, res) {
   var file = req.params[0] || '/views/index.html';
-  res.sendFile(path.join(__dirname, '../public/', file));
+  res.sendFile(path.join(__dirname, '../../public/', file));
 });
 
 module.exports = router;
